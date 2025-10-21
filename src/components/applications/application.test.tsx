6cfe694735956b65d2f9d560b359c38841c6d5c1
Howaida Sayed <h.sayed@neoxero.com>
2025-10-21 07:55:09 +0300

    getByRole Options

--- src/components/applications/application.test.tsx
+++ src/components/applications/application.test.tsx
@@ -6,10 +6,30 @@ import { Application } from "./application";
 describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
+    // screen.debug();
+    // screen.getByRole("");
     // test name
-    const nameElement = screen.getByRole("textbox", { name: "Name" });
+    // getByRole(role, options)
+    const nameElement = screen.getByRole(
+      "textbox", // role
+      { name: "Name" } // accessible name
+    );
     // assert the element is in the document
     expect(nameElement).toBeInTheDocument();
+    // getByRole(role, options)
+    const bioElement = screen.getByRole(
+      "textbox", // role
+      { name: "Bio" } // accessible name
+    );
+    // assert the element is in the document
+    expect(bioElement).toBeInTheDocument();
+
+    // test section 
+    const section1Element = screen.getByRole("heading", { level : 1, name: "Job application form" });
+    expect(section1Element).toBeInTheDocument();
+
+    const section2Element = screen.getByRole("heading", {level: 2, name: "Section 1" });
+    expect(section2Element).toBeInTheDocument();
 
     // test job location
     const jobLocationElement = screen.getByRole("combobox", { name: "Job location" });
@@ -18,7 +38,6 @@ describe("Application", () => {
     // test terms and conditions
     const termsAndConditionsElement = screen.getByRole("checkbox", { name: "I agree to the terms and conditions" });
     expect(termsAndConditionsElement).toBeInTheDocument();
-
 
     // test submit button
     const submitButtonElement = screen.getByRole("button", { name: "Submit" });
