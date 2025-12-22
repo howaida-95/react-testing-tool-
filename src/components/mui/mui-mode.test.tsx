83ad3e0fc30e935827fd709c01d385e061d335c4
Howaida Sayed <h.sayed@neoxero.com>
2025-12-22 02:09:13 +0200

    custom render function

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,13 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render, screen } from "../../test-utils";
 import { MuiMode } from "./mui-mode.tsx";
-import { AppProviders } from "../../providers/app-providers.tsx";
+
 describe("MuiMode", () => {
   test("renders text light mode", () => {
     /*
         provider inside app.tsx
         and we are rendering app.tsx here 
     */
-    render(<MuiMode />, { wrapper: AppProviders });
+    render(<MuiMode />);
     const textElement = screen.getByRole("heading");
     expect(textElement).toHaveTextContent("dark mode");
   });
